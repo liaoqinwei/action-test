47dca6512132c29155eb6c5f35dfd0d786373834
liaoqinwei <2573424017@qq.com>
2024-02-28 15:27:32 +0800

    init

---
+++ test/components/HelloWorld.test.ts
@@ -0,0 +1,22 @@
+import {mount} from "@vue/test-utils"
+import HelloWorld from "../../src/components/HelloWorld.vue";
+import {describe, expect, test} from "vitest";
+
+const wrapper = mount(HelloWorld, {
+  props: {
+    msg: "Hello World"
+  }
+})
+
+describe("test component 'HelloWorld.vue'", () => {
+  test("display prop 'msg'", () => {
+    expect(wrapper.find('h1').text()).equals("Hello World")
+  })
+
+  test("test increment count", async () => {
+    await wrapper.find(".card").find("button").trigger('click')
+    expect(wrapper.find(".card").find("button").text()).contains("count is 1")
+  })
+
+})
+
